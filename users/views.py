3112089fc509477f2ea734934fb52230470003d5
larsanik <larsanik@gmail.com>
2026-07-29 13:06:37 +0300

    Практический кейс: API для сайта театра. Старт.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm as _UserCreationForm, UsernameField
+
+from users.models import User
+
+
+class UserCreationForm(_UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {"username": UsernameField}
+
 
 # Create your views here.
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
